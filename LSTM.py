e4c492da9f10e183ce11275b19e9dac3f51adb1b
vch2128 <bonniechuang920915@gmail.com>
2024-05-30 23:26:15 +0800

    LSTM update

--- LSTM.py
+++ LSTM.py
@@ -8,19 +8,20 @@ from DataProcessing import MusicData
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_feature_dim, hidden_feature_dim, hidden_layer_num, batch_size, classes_num):
+    def __init__(self, input_dim, hidden_dim, layer_num, batch_size, output_dim):
         super(LSTM, self).__init__()
-        self.input_dim = input_feature_dim
-        self.hidden_dim = hidden_feature_dim
-        self.layer_num = hidden_layer_num
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.layer_num = layer_num
         self.batch_size = batch_size 
 
-	    # 初始化 LSTM       
-        self.lstm = nn.LSTM(input_feature_dim, hidden_feature_dim, hidden_layer_num)
+	    # initialize LSTM       
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_num)
+        # output
+        self.linear1 = nn.Linear(hidden_dim, output_dim)
 
-        # LSTM的輸出藉由單層的線性神經網路層分類
-        self.linear1 = nn.Linear(hidden_feature_dim, classes_num)
-
+    def forward(self, input, hidden=None):
+        lstm_out, hidden = self.lstm(input, hidden)
 
 
 def main():
@@ -34,9 +35,25 @@ def main():
     else:
         music_data.create_feature_data()
 
-    print(music_data.train_X.shape)
-    print(music_data.train_Y.shape)
-    #print(music_data.train_X[1])
+    # print(music_data.train_X.shape)    # (audioSample, timeSlots, features)
+    # print(music_data.train_Y.shape)
+    # print(music_data.train_X[1])
+
+    train_X = t.from_numpy(music_data.train_X).type(t.Tensor)
+    train_Y = t.from_numpy(music_data.train_Y).type(t.Tensor)
+
+    batch_size = 25
+    input_dim = 33     # the calculated features
+    hidden_dim = 128     # capture hidden features
+    layer_num = 2
+    output_dim = 12    # 12 genres
+    epoch = 300
+
+    print("Starting LSTM model...")
+    model = LSTM(input_dim, hidden_dim, layer_num, batch_size, output_dim)
+
+
+
 
 
 if __name__ == "__main__":
