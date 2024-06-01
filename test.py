e65d70017d412ec4a3ad1de56c86b983d4089aae
vch2128 <bonniechuang920915@gmail.com>
2024-06-01 13:09:56 +0800

    test data process update

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ from torch import nn
 from DataProcessing import MusicData
 from LSTM import LSTM
 
-
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
-timeseries_length = 128
 
+partition_num = 10
+timeseries_length = 1200
 
 def load_model():
     model = LSTM(input_dim=33, hidden_dim=128, batch_size=25, output_dim=12, layer_num=2)
@@ -21,55 +21,66 @@ def load_model():
         print("Model available. Loading model...")
         model.load_state_dict(torch.load(model.model_path))
     else:
-        print("No model found.")
+        print("No trained model found. Loading untrained model...")
     
     model.eval()
     return model
 
 
 def extract_feature(song):
-    data = np.zeros((1, timeseries_length, 33), dtype=np.float64)
-    y, sr = librosa.load(song)
     hop_length = 512
+    data = np.zeros((partition_num, timeseries_length, 33), dtype=np.float64)
+    y, sr = librosa.load(song)
+
     mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
     spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=hop_length)
     chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length)
     spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr, hop_length=hop_length)
 
-    data[0, :, 0:13] = mfcc.T[0:timeseries_length, :]
-    data[0, :, 13:14] = spectral_center.T[0:timeseries_length, :]
-    data[0, :, 14:26] = chroma.T[0:timeseries_length, :]
-    data[0, :, 26:33] = spectral_contrast.T[0:timeseries_length, :]
+    partition_len = int(timeseries_length / partition_num)
+    for k in range(partition_num):
+            data[k, 0:partition_len, 0:13] = mfcc.T[ k*partition_len:(k+1)*partition_len, :]
+            data[k, 0:partition_len, 13:14] = spectral_center.T[ k*partition_len:(k+1)*partition_len, :]
+            data[k, 0:partition_len, 14:26] = chroma.T[ k*partition_len:(k+1)*partition_len, :]
+            data[k, 0:partition_len, 26:33] = spectral_contrast.T[ k*partition_len:(k+1)*partition_len, :]
+    
     return data
 
 def predict(model, song):
     features = extract_feature(song)
-    features_tensor = torch.tensor(features, dtype=torch.float32).permute(1, 0, 2)
-    prediction, hidden = model(features_tensor)
-    predict_genre = MusicData().genre_list[torch.argmax(prediction)]  # max: model最認可的
+    features_tensor = torch.from_numpy(features).type(torch.Tensor)
+    outputs = model(features_tensor)
+    _, prediction = torch.max(outputs, 1)
+    genre_freq = np.zeros((1,len(MusicData().genre_list)))
+    for index, p in enumerate(prediction):
+        genre_freq[0,p] += 1
+    best_predict = genre_freq.argmax()
+    predict_genre = MusicData().genre_list[best_predict]  
     return predict_genre
 
 if __name__ == "__main__":
     model = load_model()
-    # print("Please input the folder you want to organize: ")
-    # input_song = input().strip()
-
-    # for genre_name in MusicData().genre_list:
-    #     music_folder = os.path.join(input_song, genre_name)
-    #     if not os.path.exists(music_folder):
-    #         os.makedirs(music_folder)
-
-    # genre_dict = {genre_name: [] for genre_name in MusicData().genre_list}
+    print("Model loading complete.")
+    print("Please input the folder you want to organize: ")
+    input_folder = input().strip()
     
-    # for song in os.listdir(input_song):
-    #     if song.endswith('.mp3'):
-    #         song_path = os.path.join(input_song, song)
-    #         prediction = predict(model, song_path)
-    #         target_folder = os.path.join(input_song, prediction)
-    #         shutil.move(song_path, target_folder)
-    #         genre_dict[prediction].append(song[:-4])  # 去掉.mp3
+    for genre_name in MusicData().genre_list:
+        music_folder = os.path.join(input_folder, genre_name)
+        if not os.path.exists(music_folder):
+            os.makedirs(music_folder)
 
-    # for genre, tracks in genre_dict.items():
-    #     print(f"\n{genre}: ")
-    #     for track in tracks:
-    #         print(track)
+    genre_dict = {genre_name: [] for genre_name in MusicData().genre_list}
+    
+    for song in os.listdir(input_folder):
+        if song.endswith('.mp3') or song.endswith('.wav'):
+            song_path = os.path.join(input_folder, song)
+            prediction = predict(model, song_path)
+            print(prediction)
+            target_folder = os.path.join(input_folder, prediction)
+            shutil.move(song_path, target_folder)
+            genre_dict[prediction].append(song[:-4])  # 去掉.mp3
+
+    for genre, tracks in genre_dict.items():
+        print(f"\n{genre}: ")
+        for track in tracks:
+            print(track)
